eec538c299b9f8a9db99413efc68e8c2b18cc462
mamutla3-1982 <luismamutla3@gmail.com>
2026-02-12 15:23:36 +0100

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,12 @@
+// Inicializar el mapa
+var map = L.map('map').setView([37.8882, -4.7794], 12); // Córdoba como ejemplo
 
+// Capa base de OpenStreetMap (puedes cambiarla luego por SIGPAC)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Mensaje de prueba
+console.log("Mapa cargado correctamente");
+``
