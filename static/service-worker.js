b6a3bf2b5bf9a1a77baa715be3eb08aae945ce14
mamutla3-1982 <luismamutla3@gmail.com>
2026-02-12 12:11:19 +0100

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'agro-v1';
-const ASSETS = [
+const CACHE_NAME = 'agro-v2';
+const FILES = [
   '/',
-  '/static/manifest.json',
+  '/manifest.json',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
