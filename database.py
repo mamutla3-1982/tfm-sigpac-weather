d4848feba2ad4078387356f6bd34865b0bce43d7
mamutla3-1982 <luismamutla3@gmail.com>
2026-02-12 17:32:07 +0100

    Create database.py

---
+++ database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine, Column, Integer, String, JSON, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+import os
+
+# Leer la URL desde Render
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    email = Column(String, unique=True)
+    password_hash = Column(String)
+
+    parcelas = relationship("Parcela", back_populates="usuario")
+
+
+class Parcela(Base):
+    __tablename__ = "parcelas"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("usuarios.id"))
+    nombre = Column(String)
+    provincia = Column(String)
+    municipio = Column(String)
+    cultivo = Column(String)
+    superficie = Column(String)
+    geometria = Column(JSON)
+
+    usuario = relationship("Usuario", back_populates="parcelas")
+
+
+def init_db():
+    Base.metadata.create_all(engine)
